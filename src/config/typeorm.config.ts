fbb9eaece14137df99bc03002466f2e09adef401
Al Mobin <instance.of.venture@gmail.com>
2023-01-19 10:53:15 +0600

    Basic code scaffolding

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,32 @@
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import {
+    TypeOrmModuleAsyncOptions,
+    TypeOrmModuleOptions
+} from '@nestjs/typeorm'
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (
+        configService: ConfigService
+    ): Promise<TypeOrmModuleOptions> => {
+        return {
+            type: 'postgres',
+            host: configService.get('POSTGRES_HOST', 'localhost'),
+            port: configService.get<number>('POSTGRES_PORT', 5432),
+            username: configService.get('DB_USERNAME', 'postgres'),
+            password: configService.get('DB_PASSWORD', 'password'),
+            database: configService.get(
+                'DB_NAME',
+                'anchorblock-accounting-mvp'
+            ),
+            entities: [__dirname + '/../**/*.entity.{js,ts}'],
+            migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
+            extra: {
+                charset: 'utf8mb4_unicode_ci'
+            },
+            synchronize: true,
+            logging: false
+        }
+    }
+}
